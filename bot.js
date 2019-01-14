01bdfa98af10bc3227a732bb7fe92db834291f87
Zino <41270843+zungf@users.noreply.github.com>
2019-01-14 12:46:50 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const fs = require('fs');
+const getYoutubeID = require('get-youtube-id');
+const moment = require('moment');
+const db = require('quick.db');
+const client = new Discord.Client();  
+const giphy = require('giphy-api')();    
+const googl = require('goo.gl');  
+const translate = require('google-translate-api');  
+const UserBlocked = new Set();
+const jimp = require('jimp');  
+const math = require('math-expression-evaluator');
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it');
+const queue = new Map();
+const zalgo = require('zalgolize');  
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const sql = require("sqlite");
+ const dateFormat = require('dateformat');
+ const pretty = require('pretty-ms')
+const sWlc = {}
+
+const prefix = "Z"
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '434666573370753024') return;
+
+if (message.content.startsWith(prefix + 'p')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
